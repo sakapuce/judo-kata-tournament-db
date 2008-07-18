91d6fae427f88808398fd04c610deebff5685b54
geofrey.van.hecke <geofrey.van.hecke@0e8faaa3-6e50-0410-be6c-23981ba65068>
2008-07-18 12:28:51 +0000

    Change the way the DataTableHelper constructor initialize the inner DbDataAdapter. The functions SetAdapter and CreateDefaultAdapter were also modified.

--- DataAccessLayer/DataTableHelper.cs
+++ DataAccessLayer/DataTableHelper.cs
@@ -15,12 +15,19 @@ namespace DALHelper
             private set { _dataTable = value; }
         }
     
+        /// <summary>
+        /// Create a new instance of DataTableHelper initialized with a default DbDataAdapter.
+        /// </summary>
+        /// <param name="dataTable">Returns a new instance of DataTableHelper</param>
         public DataTableHelper(DataTable dataTable)
         {
             Table = dataTable;
             _adapter = CreateDefaultAdapter();
         }
 
+        /// <summary>
+        /// Returns the DbDataAdapter instance used by the current DataTableHelper object
+        /// </summary>
         public DbDataAdapter Adapter
         {
             get
@@ -34,8 +41,19 @@ namespace DALHelper
             }
         }
 
+        /// <summary>
+        /// Set the DbCommand for the SELECT, UPDATE, INSERT and DELETE statements used by the current instance of DataTableHelper. 
+        /// </summary>
+        /// <param name="selectCommand">a DbCommand for the SELECT statement</param>
+        /// <param name="updateCommand">a DbCommand for the SELECT statement</param>
+        /// <param name="insertCommand">a DbCommand for the SELECT statement</param>
+        /// <param name="deleteCommand">a DbCommand for the SELECT statement</param>
+        /// <returns>a DbDataAdapter instance initialized with the specified DbCommands.</returns>
         public DbDataAdapter SetAdapter(DbCommand selectCommand, DbCommand updateCommand, DbCommand insertCommand, DbCommand deleteCommand)
         {
+            if (_adapter == null) 
+                _adapter = DbHelper.CreateAdapter();
+
             _adapter.SelectCommand = selectCommand;
             _adapter.InsertCommand = insertCommand;
             _adapter.UpdateCommand = updateCommand;
@@ -44,6 +62,41 @@ namespace DALHelper
             return _adapter;
         }
 
+        /// <summary>
+        /// Create and Set the inner DbDataAdapter by using the selectCommand provided.
+        /// This function will automaticaly generates the INSERT, UPDATE and DELETE commands
+        /// based on the schema returned by the SELECT command.
+        /// </summary>
+        /// <param name="selectCommand">a DBCommand used to generates automatically the INSERT, UPDATE and DELETE commands of the inner DbDataAdapter</param>
+        /// <returns>the instance of the inner DbDataAdapter initialiezed from the SELECT DbCommand provided in parameter.</returns>
+        public DbDataAdapter SetAdapter(DbCommand selectCommand)
+        {
+            _adapter = CreateDefaultAdapter(selectCommand);
+
+            return _adapter;
+        }
+
+        /// <summary>
+        /// Create and Set the inner DbDataAdapter by using the select query provided.
+        /// This function will automaticaly generates the INSERT, UPDATE and DELETE commands
+        /// based on the schema returned by the SELECT query.
+        /// </summary>
+        /// <param name="selectQuery">a String used to generates automatically the INSERT, UPDATE and DELETE commands of the inner DbDataAdapter</param>
+        /// <returns>the instance of the inner DbDataAdapter initialiezed from the SELECT query provided in parameter.</returns>
+        public DbDataAdapter SetAdapter(string selectQuery)
+        {
+            _adapter = CreateDefaultAdapter(DbHelper.CreateCommand(selectQuery));
+            return _adapter;
+        }
+
+        /// <summary>
+        /// Set the queries for the SELECT, UPDATE, INSERT and DELETE statements used by the current instance of DataTableHelper. 
+        /// </summary>
+        /// <param name="selectQuery">a string with the SELECT statement</param>
+        /// <param name="updateQuery">a string with the SELECT statement</param>
+        /// <param name="insertQuery">a string with the SELECT statement</param>
+        /// <param name="deleteQuery">a string with the SELECT statement</param>
+        /// <returns>a DbDataAdapter instance initialized with the specified queries.</returns>
         public DbDataAdapter SetAdapter(string selectQuery, string updateQuery, string insertQuery, string deleteQuery)
         {
             DbCommand selectCommand = DbHelper.CreateCommand(selectQuery);
@@ -59,7 +112,7 @@ namespace DALHelper
             DbDataAdapter dbAdapter = DbHelper.CreateAdapter();
             DbCommandBuilder dbCommandBuilder = DbHelper.CreateDbCommandBuilder();
 
-            dbAdapter.SelectCommand = DbHelper.CreateCommand(CreateSelectQuery());
+            dbAdapter.SelectCommand = DbHelper.CreateCommand(DefaultSelectQuery);
 
             dbCommandBuilder.DataAdapter = dbAdapter;
 
@@ -71,14 +124,41 @@ namespace DALHelper
             return dbAdapter;
         }
 
-        public virtual string CreateSelectQuery()
+
+        private static DbDataAdapter CreateDefaultAdapter(DbCommand selectCommand)
         {
-            if (Table.Columns.Count == 0)
+            // WHY A STATIC METHOD ?
+            //This method has been set as static because it does not change any instance properties or field.
+            //After doing so, the compiler will emit non-virtual call sites to these members which will prevent
+            //a check at runtime for each call that insures the current object pointer is non-null. This can result 
+            //in a measurable performance gain for performance-sensitive code.
+
+            DbDataAdapter dbAdapter = DbHelper.CreateAdapter();
+            DbCommandBuilder dbCommandBuilder = DbHelper.CreateDbCommandBuilder();
+
+            dbAdapter.SelectCommand = selectCommand;
+
+            dbCommandBuilder.DataAdapter = dbAdapter;
+
+
+            dbAdapter.InsertCommand = dbCommandBuilder.GetInsertCommand();
+            dbAdapter.UpdateCommand = dbCommandBuilder.GetUpdateCommand();
+            dbAdapter.DeleteCommand = dbCommandBuilder.GetDeleteCommand();
+
+            return dbAdapter;
+        }
+
+        public virtual string DefaultSelectQuery
+        {
+            get
             {
-                throw new InvalidOperationException("The current Table contains no DataColumn. The 'SELECT' command cannot be created.");
+                if (Table.Columns.Count == 0)
+                {
+                    throw new InvalidOperationException("The current Table contains no DataColumn. The 'SELECT' command cannot be created.");
+                }
+                string fields = string.Join(", ", GetColumnNames());
+                return string.Format("SELECT {0} FROM {1}", fields, Table.TableName);   
             }
-            string fields = string.Join(", ", GetColumnNames());
-            return string.Format("SELECT {0} FROM {1}", fields, Table.TableName);
         }
 
         public virtual string[] GetColumnNames()
